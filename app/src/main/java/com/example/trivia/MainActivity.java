555702cb024efef525f4ec8c2d3d3116bb792582
steph <stephdellbre@gmail.com>
2019-04-10 07:28:07 -0600

    Cargar Carpeta

---
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.trivia;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button preg1;
+    Button preg2;
+    Button preg3;
+    Button preg4;
+    Button sig;
+    ImageView imageView;
+    String url="https://i.blogs.es/4e1365/pikach/450_1000.jpg";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preg1= (Button)findViewById(R.id.preg1);
+        preg2= (Button)findViewById(R.id.preg2);
+        preg3= (Button)findViewById(R.id.preg3);
+        preg4= (Button)findViewById(R.id.preg4);
+        sig= (Button)findViewById(R.id.sig);
+        imageView=(ImageView)findViewById(R.id.imageView);
+
+
+
+        preg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Primer Pregunta", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        preg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Segunda Pregunta", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        preg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Tercer Pregunta", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        preg4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Cuarta Pregunta", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
+
+
+}
